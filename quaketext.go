c2cc841d5e240c36f295b19033ce8dc7d81dc1ba
Viktor Persson <viktor.persson@arcsin.se>
2022-04-26 02:14:49 +0200

    Update quaketext translation.

--- quaketext.go
+++ quaketext.go
@@ -1,22 +1,35 @@
 package serverstat
 
-import "strings"
+import (
+	"strings"
+)
 
 func quakeTextToPlainText(quakeText string) string {
-	readableTextBytes := []byte(quakeText)
-
-	var charset = [...]byte{
-		' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-		'[', ']', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', ' ', ' ', ' ',
+	var charset = [128]string{
+		"<", "=", ">", "#", "#", ".", "#", "#",
+		"#", "#", " ", "#", " ", ">", ".", ".",
+		"[", "]", "0", "1", "2", "3", "4", "5",
+		"6", "7", "8", "9", "•", "<", "=", ">",
+		"•", "!", "\"", "#", "$", "%", "&", "\"",
+		"(", ")", "*", "+", ",", "-", ".", "/",
+		"0", "1", "2", "3", "4", "5", "6", "7",
+		"8", "9", ":", ";", "<", "=", ">", "?",
+		"@", "A", "B", "C", "D", "E", "F", "G",
+		"H", "I", "J", "K", "L", "M", "N", "O",
+		"P", "Q", "R", "S", "T", "U", "V", "W",
+		"X", "Y", "Z", "[", "\\", "]", "^", "_",
+		"`", "a", "b", "c", "d", "e", "f", "g",
+		"h", "i", "j", "k", "l", "m", "n", "o",
+		"p", "q", "r", "s", "t", "u", "v", "w",
+		"x", "y", "z", "{", "|", "}", "~", "<",
 	}
 
-	for i := range quakeText {
-		readableTextBytes[i] &= 0x7f
+	translation := ""
 
-		if quakeText[i] < byte(len(charset)) {
-			readableTextBytes[i] = charset[quakeText[i]]
-		}
+	for _, charByte := range []byte(quakeText) {
+		charByte &= 0x7f // strip color (> 128)
+		translation += charset[charByte]
 	}
 
-	return strings.TrimSpace(string(readableTextBytes))
+	return strings.TrimSpace(translation)
 }
