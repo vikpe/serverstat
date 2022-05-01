99f151481335b7ba871cb4ec3870fffb4fba1b89
Viktor Persson <viktor.persson@arcsin.se>
2022-05-01 20:15:33 +0200

    Refactor quake char to plain string.

--- quaketext/quakechar/quakechar.go
+++ quaketext/quakechar/quakechar.go
@@ -13,17 +13,19 @@ func (qchar QuakeChar) RemoveColor() *QuakeChar {
 }
 
 func (qchar QuakeChar) ToPlainString() string {
+	plainByte := qchar.RemoveColor().Byte
+
+	if plainByte == 127 { // weird left arrow at end of charset
+		return ""
+	}
+
 	charsetToprows := []string{
 		"•", "", "", "", "", "•", "", "", "", "", "", "", "", "", "•", "•",
 		"[", "]", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "•", "", "", "",
 	}
 	charsetTopRowsSize := byte(len(charsetToprows))
 
-	plainByte := qchar.RemoveColor().Byte
-
-	if plainByte == 127 { // weird left arrow at end of charset
-		return ""
-	} else if plainByte < charsetTopRowsSize {
+	if plainByte < charsetTopRowsSize {
 		return charsetToprows[plainByte]
 	} else {
 		return string(plainByte)
