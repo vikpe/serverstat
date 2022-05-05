defa451991e4367fa8be03bfac2d1e115c836d22
Viktor Persson <viktor.persson@arcsin.se>
2022-05-05 17:22:40 +0200

    Refactor settings, add tests.

--- qserver/qsettings/qsettings.go
+++ qserver/qsettings/qsettings.go
@@ -4,7 +4,7 @@ import "strings"
 
 func New(settingsString string) map[string]string {
 	settingsLines := strings.FieldsFunc(settingsString, func(r rune) bool { return r == '\\' })
-	settings := make(map[string]string, len(settingsLines))
+	settings := make(map[string]string, 0)
 
 	for i := 0; i < len(settingsLines)-1; i += 2 {
 		settings[settingsLines[i]] = settingsLines[i+1]
--- qserver/qsettings/qsettings_test.go
+++ qserver/qsettings/qsettings_test.go
@@ -1,1 +1,24 @@
 package qsettings_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vikpe/serverstat/qserver/qsettings"
+)
+
+func TestNew(t *testing.T) {
+	// empty
+	assert.Equal(t, make(map[string]string, 0), qsettings.New(""))
+
+	// non-empty
+	expect := map[string]string{
+		"*version":  "MVDSV 0.35-dev",
+		"maxfps":    "77",
+		"pm_ktjump": "1",
+	}
+	settingsString := `maxfps\77\pm_ktjump\1\*version\MVDSV 0.35-dev`
+	actual := qsettings.New(settingsString)
+
+	assert.Equal(t, expect, actual)
+}
