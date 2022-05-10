4322aa719924ee4bf1bd1af312b648585fd7b364
Viktor Persson <viktor.persson@arcsin.se>
2022-05-10 14:06:58 +0200

    Add Mode type.

---
+++ qserver/mvdsv/qmode/qmode.go
@@ -0,0 +1,56 @@
+package qmode
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/vikpe/serverstat/qserver/qsettings"
+)
+
+type Mode string
+
+func (m Mode) Is(name string) bool {
+	return strings.ToLower(name) == string(m)
+}
+func (m Mode) Is1on1() bool     { return m.Is("1on1") }
+func (m Mode) Is2on2() bool     { return m.Is("2on2") }
+func (m Mode) Is3on3() bool     { return m.Is("3on3") }
+func (m Mode) Is4on4() bool     { return m.Is("4on4") }
+func (m Mode) IsRace() bool     { return m.Is("race") }
+func (m Mode) IsFfa() bool      { return m.Is("ffa") }
+func (m Mode) IsCtf() bool      { return m.Is("ctf") }
+func (m Mode) IsCoop() bool     { return m.Is("coop") }
+func (m Mode) IsCustom() bool   { return m.Is("custom") }
+func (m Mode) IsFortress() bool { return m.Is("fortress") }
+func (m Mode) IsUnknown() bool  { return m.Is("unknown") }
+
+func Parse(settings qsettings.Settings) Mode {
+	gameDir := settings.Get("*gamedir", "unknown")
+
+	if "qw" != gameDir {
+		return Mode(gameDir)
+	}
+
+	if settings.Has("ktxmode") {
+		return Mode(settings.Get("ktxmode", "unknown"))
+	}
+
+	teamplay := settings.GetInt("teamplay", 0)
+	maxClients := settings.GetInt("maxclients", 0)
+
+	if teamplay > 0 {
+		if 2 == teamplay && 26 == maxClients {
+			return "coop"
+		} else {
+			playersPerTeam := maxClients / 2
+			modeName := fmt.Sprintf("%don%d", playersPerTeam, playersPerTeam)
+			return Mode(modeName)
+		}
+	}
+
+	if 2 == maxClients {
+		return "1on1"
+	} else {
+		return "ffa"
+	}
+}
---
+++ qserver/mvdsv/qmode/qmode_test.go
@@ -0,0 +1,56 @@
+package qmode_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vikpe/serverstat/qserver/mvdsv/qmode"
+	"github.com/vikpe/serverstat/qserver/qsettings"
+)
+
+func TestParse(t *testing.T) {
+	testCases := map[string]qsettings.Settings{
+		"fortress": {"*gamedir": "fortress"},
+		"race":     {"*gamedir": "qw", "ktxmode": "race"},
+		"coop":     {"*gamedir": "qw", "teamplay": "2", "maxclients": "26"},
+		"1on1":     {"*gamedir": "qw", "maxclients": "2"},
+		"2on2":     {"*gamedir": "qw", "maxclients": "4", "teamplay": "2"},
+		"4on4":     {"*gamedir": "qw", "maxclients": "8", "teamplay": "2"},
+		"ffa":      {"*gamedir": "qw", "maxclients": "8"},
+	}
+
+	for expectedModeName, settings := range testCases {
+		t.Run(expectedModeName, func(t *testing.T) {
+			expect := qmode.Mode(expectedModeName)
+			assert.Equal(t, expect, qmode.Parse(settings), expectedModeName)
+		})
+	}
+}
+
+func TestModeValidators(t *testing.T) {
+	testCases := map[string]func(m qmode.Mode) bool{
+		"1on1":     func(m qmode.Mode) bool { return m.Is1on1() },
+		"2on2":     func(m qmode.Mode) bool { return m.Is2on2() },
+		"3on3":     func(m qmode.Mode) bool { return m.Is3on3() },
+		"4on4":     func(m qmode.Mode) bool { return m.Is4on4() },
+		"race":     func(m qmode.Mode) bool { return m.IsRace() },
+		"ffa":      func(m qmode.Mode) bool { return m.IsFfa() },
+		"ctf":      func(m qmode.Mode) bool { return m.IsCtf() },
+		"coop":     func(m qmode.Mode) bool { return m.IsCoop() },
+		"custom":   func(m qmode.Mode) bool { return m.IsCustom() },
+		"fortress": func(m qmode.Mode) bool { return m.IsFortress() },
+		"unknown":  func(m qmode.Mode) bool { return m.IsUnknown() },
+	}
+
+	for currentModeName, currentValidator := range testCases {
+		for modeName, otherValidator := range testCases {
+			mode := qmode.Mode(currentModeName)
+
+			if modeName == currentModeName {
+				assert.True(t, currentValidator(mode))
+			} else {
+				assert.False(t, otherValidator(mode))
+			}
+		}
+	}
+}
