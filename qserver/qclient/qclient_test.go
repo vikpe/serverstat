54e39af3ab8361681adaa1aa677656996b93ca35
Viktor Persson <viktor.persson@arcsin.se>
2022-06-16 23:08:25 +0200

    Update isBot method.

--- qserver/qclient/qclient_test.go
+++ qserver/qclient/qclient_test.go
@@ -113,7 +113,7 @@ func TestClient_IsPlayer(t *testing.T) {
 }
 
 func TestClient_IsBot(t *testing.T) {
-	assert.True(t, qclient.Client{Name: qstring.New("XantoM"), Ping: -666}.IsBot())  // bot ping
+	assert.True(t, qclient.Client{Name: qstring.New("XantoM"), Ping: 10}.IsBot())    // bot ping
 	assert.True(t, qclient.Client{Name: qstring.New("[ServeMe]"), Ping: 12}.IsBot()) // bot name
 	assert.False(t, qclient.Client{Name: qstring.New("XantoM"), Ping: 12}.IsBot())   // neither
 	assert.False(t, qclient.Client{Name: qstring.New("t"), Ping: 12}.IsBot())        // neither
